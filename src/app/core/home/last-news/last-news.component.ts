f646adff6e1968a3094554eeddebca9f386e672d
Chlafen <bouchnak.amine03@gmail.com>
2024-01-22 18:08:22 +0100

    finalized  home news

--- src/app/core/home/last-news/last-news.component.ts
+++ src/app/core/home/last-news/last-news.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-last-news',
   templateUrl: './last-news.component.html',
-  styleUrls: ['./last-news.component.sass']
+  styleUrls: ['./last-news.component.sass'],
 })
 export class LastNewsComponent {
-
+  news: any[] = [
+    {
+      title:
+        'Real madrid wins the champions league again after 3 years of waiting for the 14th title in the history of the club',
+      image:
+        'https://latest.fotmob.com/wp-content/uploads/2024/01/Covers_De-Rossi-2201-1.png',
+      date: '2021-05-29',
+    },
+    {
+      title:
+        'Real madrid wins the champions league again after 3 years of waiting for the 14th title in the history of the club',
+      image:
+        'https://latest.fotmob.com/wp-content/uploads/2024/01/Covers_De-Rossi-2201-1.png',
+      date: '2021-05-29',
+    },
+    {
+      title:
+        'Real madrid wins the champions league again after 3 years of waiting for the 14th title in the history of the club',
+      image:
+        'https://latest.fotmob.com/wp-content/uploads/2024/01/Covers_De-Rossi-2201-1.png',
+      date: '2021-05-29',
+    },
+    {
+      title:
+        'Real madrid wins the champions league again after 3 years of waiting for the 14th title in the history of the club',
+      image:
+        'https://latest.fotmob.com/wp-content/uploads/2024/01/Covers_De-Rossi-2201-1.png',
+      date: '2021-05-29',
+    },
+  ];
 }
