e7209f40bedc4708d034bc241570f3c6f436f54d
Chlafen <bouchnak.amine03@gmail.com>
2024-01-23 13:41:59 +0100

    ui fixes

--- src/app/home/views/top-transfers/top-transfers.component.ts
+++ src/app/home/views/top-transfers/top-transfers.component.ts
@@ -29,5 +29,27 @@ export class TopTransfersComponent {
         'https://resources.premierleague.com/premierleague/badges/50/t1.png',
       price: '€15.00M',
     },
+    {
+      name: 'Lionel Messi',
+      img: 'https://resources.premierleague.com/premierleague/photos/players/40x40/p200785.png',
+      from: 'FC Barcelona',
+      to: 'Paris Saint-Germain',
+      fromImg:
+        'https://resources.premierleague.com/premierleague/badges/50/t9.png',
+      toImg:
+        'https://resources.premierleague.com/premierleague/badges/50/t3.png',
+      price: '€8.00M',
+    },
+    {
+      name: 'Cristiano Ronaldo',
+      img: 'https://resources.premierleague.com/premierleague/photos/players/40x40/p37265.png',
+      from: 'Juventus',
+      to: 'Manchester United',
+      fromImg:
+        'https://resources.premierleague.com/premierleague/badges/50/t37.png',
+      toImg:
+        'https://resources.premierleague.com/premierleague/badges/50/t1.png',
+      price: '€15.00M',
+    },
   ];
 }
