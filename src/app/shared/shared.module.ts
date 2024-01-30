b907bcc940b6a0e830cfd850deaca390cc5035b3
Chlafen <bouchnak.amine03@gmail.com>
2024-01-30 22:09:11 +0100

    fix merge errors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,6 @@ import { ArrowRightSolidComponent } from './views/arrow-right-solid/arrow-right-
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LoadingComponent } from './views/loading/loading.component';
-import { TransfersTableComponent } from './views/transfers-table/transfers-table.component';
-import { PlayerTransferDataComponent } from './views/player-transfer-data/player-transfer-data.component';
-import { RouterModule } from '@angular/router';
-import { NewsCardLargeComponent } from './views/news-card-large/news-card-large.component';
-import { NewsCardComponent } from './views/news-card/news-card.component';
 import { LeagueImagePipe } from './views/league-image/league-image.pipe';
 import { CountryImagePipe } from './views/country-image/country-image.pipe';
 import { CountryFallbackImageDirective } from './views/country-fallback-image/country-fallback-image.directive';
