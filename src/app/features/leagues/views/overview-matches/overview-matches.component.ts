b76a99633f67842801283008f5512743d9295088
M0ngi <31113271+M0ngi@users.noreply.github.com>
2024-02-04 13:51:00 +0100

    Add trackBy to ngx-slick items (#16)

--- src/app/features/leagues/views/overview-matches/overview-matches.component.ts
+++ src/app/features/leagues/views/overview-matches/overview-matches.component.ts
@@ -1,20 +1,19 @@
 import { Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { SlickCarouselComponent } from 'ngx-slick-carousel';
 import { LeagueOverviewMatch } from 'src/app/common/types/league';
+import { Match } from 'src/app/common/types/matches';
 
 @Component({
   selector: 'app-overview-matches',
   templateUrl: './overview-matches.component.html',
   styleUrls: ['./overview-matches.component.sass'],
 })
-export class OverviewMatchesComponent implements OnChanges {
+export class OverviewMatchesComponent {
   @Input() matches!: LeagueOverviewMatch[];
   @Input() gotoIdx!: number;
-  @ViewChild('slickModal') slickModal!: SlickCarouselComponent;
 
   windowWidth: number = window.innerWidth;
   slideConfig = {
-    initialSlide: this.gotoIdx,
     infinite: false,
     slidesToShow: 3,
     slidesToScroll: 3,
@@ -36,14 +35,7 @@ export class OverviewMatchesComponent implements OnChanges {
     ],
   };
 
-  ngOnChanges() {
-    console.log(this.slickModal)
-    console.log(this.gotoIdx)
-    if (this.slickModal) this.slickModal.slickGoTo(5)
-
-  }
-
-  afterChange() {
-    console.log('afterChange');
+  identify(_: number, item: LeagueOverviewMatch) {
+    return item.id;
   }
 }
